7e4b8d7699a1482ab83205a445d2137b3a50861b
Erjan Kalybek <erjanmx@gmail.com>
2019-09-09 17:09:26 +0200

    Add elastiCubes methods (#10)

--- src/Api/v0.9/ElastiCubes.php
+++ src/Api/v0.9/ElastiCubes.php
@@ -3,7 +3,6 @@
 namespace Sisense\Api\V09;
 
 use Sisense\Api\AbstractApi;
-use Sisense\Exceptions\MethodNotImplemented;
 
 /**
  * Class ElastiCubes
@@ -80,33 +79,317 @@ class ElastiCubes extends AbstractApi
     /**
      * Returns ElastiCubes with their server and ElastiCube details.
      *
+     * @param array $parameters
      * @return array
-     * @throws MethodNotImplemented
      */
-    public function getAll() : array
+    public function getAll(array $parameters = []) : array
     {
-        throw new MethodNotImplemented();
+        return $this->get($this->getPath(), $parameters);
     }
 
     /**
      * Returns the ElastiCube servers with metadata.
      *
+     * @param array $parameters
      * @return array
-     * @throws MethodNotImplemented
      */
-    public function getServers() : array
+    public function getServers(array $parameters = []) : array
     {
-        throw new MethodNotImplemented();
+        $path = $this->getPath('servers');
+
+        return $this->get($path, $parameters);
     }
 
     /**
      * Returns all the ElastiCubes by server.
      *
+     * @param string $server
+     * @param array $parameters
      * @return array
-     * @throws MethodNotImplemented
      */
-    public function getServer() : array
+    public function getServer(string $server, array $parameters = []) : array
     {
-        throw new MethodNotImplemented();
+        $path = $this->getPath(sprintf('%s/%s', 'servers', $server));
+
+        return $this->get($path, $parameters);
+    }
+
+    /**
+     * Returns ElastiCube metadata by entering the server name/address/IP and database name.
+     *
+     * @param string $server
+     * @param array $parameters
+     * @return array
+     */
+    public function getServerSimple(string $server, array $parameters = []) : array
+    {
+        $path = $this->getPath(sprintf('%s/%s/simple', 'servers', $server));
+
+        return $this->get($path, $parameters);
+    }
+
+    /**
+     * Returns the status of each ElastiCube in the selected server.
+     *
+     * @param string $server
+     * @param array $parameters
+     * @return array
+     */
+    public function getServerStatus(string $server, array $parameters = []) : array
+    {
+        $path = $this->getPath(sprintf('%s/%s/status', 'servers', $server));
+
+        return $this->get($path, $parameters);
+    }
+
+    /**
+     * Returns the data security rules set up for the ElastiCube.
+     *
+     * @param string $server
+     * @param string $elasticCube
+     * @param array $parameters
+     * @return array
+     */
+    public function getDataSecurityRules(string $server, string $elasticCube, array $parameters = []) : array
+    {
+        $path = $this->getPath(sprintf('%s/%s/datasecurity', $server, $elasticCube));
+
+        return $this->get($path, $parameters);
+    }
+
+    /**
+     * Returns data security for a specific user in a specific ElastiCube.
+     *
+     * @param string $server
+     * @param string $cube
+     * @param string $user
+     * @return array
+     */
+    public function getUserDataSecurity(string $server, string $cube, string $user) : array
+    {
+        $path = $this->getPath(sprintf('%s/%s/%s/datasecurity', $server, $cube, $user));
+
+        return $this->get($path);
+    }
+
+    /**
+     * Returns ElastiCube data security for a column in a table in the ElastiCube.
+     *
+     * @param string $server
+     * @param string $elastiCube
+     * @param string $table
+     * @param string $column
+     * @return array
+     */
+    public function getColumnDataSecurity(string $server, string $elastiCube, string $table, string $column) : array
+    {
+        $path = $this->getPath(sprintf('%s/%s/datasecurity/%s/%s', $server, $elastiCube, $table, $column));
+
+        return $this->get($path);
+    }
+
+    /**
+     * Returns all authentication records for the given ElastiCube.
+     *
+     * @param string $server
+     * @param string $elastiCube
+     * @return array
+     */
+    public function getAuthRecords(string $server, string $elastiCube) : array
+    {
+        $path = $this->getPath(sprintf('%s/%s/permissions', $server, $elastiCube));
+
+        return $this->get($path);
+    }
+
+    /**
+     * Starts the ElastiCube Server.
+     *
+     * @param string $server
+     * @param string $elastiCube
+     * @return array
+     */
+    public function startServer(string $server, string $elastiCube) : array
+    {
+        $path = $this->getPath(sprintf('%s/%s/start', $server, $elastiCube));
+
+        return $this->post($path);
+    }
+
+    /**
+     * Stops the ElastiCube Server.
+     *
+     * @param string $server
+     * @param string $elastiCube
+     * @return array
+     */
+    public function stopServer(string $server, string $elastiCube) : array
+    {
+        $path = $this->getPath(sprintf('%s/%s/stop', $server, $elastiCube));
+
+        return $this->post($path);
+    }
+
+    /**
+     * Restarts the ElastiCube server.
+     *
+     * @param string $server
+     * @param string $elastiCube
+     * @return array
+     */
+    public function restartServer(string $server, string $elastiCube) : array
+    {
+        $path = $this->getPath(sprintf('%s/%s/restart', $server, $elastiCube));
+
+        return $this->post($path);
+    }
+
+    /**
+     * Starts the build process for an ElastiCube.
+     *
+     * @param string $server
+     * @param string $elastiCube
+     * @param string $type
+     * @return array
+     */
+    public function startBuild(string $server, string $elastiCube, string $type = '') : array
+    {
+        $path = $this->getPath(sprintf('%s/%s/startBuild', $server, $elastiCube));
+
+        return $this->post($path, ['type' => $type]);
+    }
+
+    /**
+     * Stops the build process for an ElastiCube.
+     *
+     * @param string $server
+     * @param string $elastiCube
+     * @return array
+     */
+    public function stopBuild(string $server, string $elastiCube) : array
+    {
+        $path = $this->getPath(sprintf('%s/%s/stopBuild', $server, $elastiCube));
+
+        return $this->post($path);
+    }
+
+    /**
+     * Executes a JAQL query on the ElastiCube.
+     *
+     * @param string $jaql
+     * @param string $elastiCube
+     * @return array
+     */
+    public function executeJAQL(string $jaql, string $elastiCube) : array
+    {
+        $path = $this->getPath(sprintf('%s/jaql', $elastiCube));
+
+        return $this->post($path, ['jaql' => $jaql]);
+    }
+
+    /**
+     * Defines data security for an ElastiCube.
+     *
+     * @param string $server
+     * @param string $elastiCube
+     * @param array $body
+     * @return array
+     */
+    public function defineDataSecurity(string $server, string $elastiCube, array $body) : array
+    {
+        $path = $this->getPath(sprintf('%s/%s/datasecurity', $server, $elastiCube));
+
+        return $this->post($path, ['body' => $body]);
+    }
+
+    /**
+     * Adds new data security for ElastiCube.
+     *
+     * @param array $parameters
+     * @return array
+     */
+    public function addDataSecurity(array $parameters) : array
+    {
+        $path = $this->getPath('datasecurity');
+
+        return $this->post($path, $parameters);
+    }
+
+    /**
+     * Attaches and detaches an ElastiCube folder to a server.
+     *
+     * @param string $server
+     * @param string $elastiCube
+     * @param array $body
+     * @return array
+     */
+    public function attachDetachFolder(string $server, string $elastiCube, array $body = []) : array
+    {
+        $path = $this->getPath(sprintf('%s/%s/attachDetach', $server, $elastiCube));
+
+
+        return $this->post($path, ['body' => $body]);
+    }
+
+    /**
+     * Defines a new permission for the given ElastiCube.
+     *
+     * @param string $server
+     * @param string $elastiCube
+     * @param array $shares
+     * @return array
+     */
+    public function definePermission(string $server, string $elastiCube, array $shares = []) : array
+    {
+        $path = $this->getPath(sprintf('%s/%s/permissions', $server, $elastiCube));
+
+        return $this->post($path, ['shares' => $shares]);
+    }
+
+    /**
+     * Updates the permissions (ACLs) for the current ElastiCube.
+     *
+     * @param string $server
+     * @param string $elastiCube
+     * @param array $shares
+     * @return array
+     */
+    public function updatePermissions(string $server, string $elastiCube, array $shares = []) : array
+    {
+        $path = $this->getPath(sprintf('%s/%s/permissions', $server, $elastiCube));
+
+        return $this->put($path, ['shares' => $shares]);
+    }
+
+    /**
+     * Deletes data context from a column in an ElastiCube table.
+     *
+     * @param string $server
+     * @param string $elastiCube
+     * @param string $table
+     * @param string $column
+     * @return array
+     */
+    public function deleteDataContext(string $server, string $elastiCube, string $table, string $column) : array
+    {
+        $path = $this->getPath(sprintf('%s/%s/datasecurity/%s/%s', $server, $elastiCube, $table, $column));
+
+        return $this->delete($path);
+    }
+
+    /**
+     * Deletes all permissions for the ElastiCube.
+     *
+     * @param string $server
+     * @param string $elastiCube
+     * @param string $table
+     * @param string $column
+     * @return array
+     */
+    public function deleteAllPermissions(string $server, string $elastiCube, string $table, string $column) : array
+    {
+        $path = $this->getPath(sprintf('%s/%s/datasecurity/%s/%s', $server, $elastiCube, $table, $column));
+
+        return $this->delete($path);
     }
 }
--- tests/Api/v0.9/ElastiCubesTest.php
+++ tests/Api/v0.9/ElastiCubesTest.php
@@ -86,4 +86,290 @@ class ElastiCubesTest extends TestCase
 
         $this->clientMock->elastiCubes->executeSql($elastiCube);
     }
+
+    /**
+     * @covers \Sisense\Api\V09\ElastiCubes::getAll()
+     */
+    public function testGetAll()
+    {
+        $this->clientMock->expects($this->once())
+            ->method('runRequest')
+            ->with('elasticubes/', 'GET', ['query' => []])
+            ->willReturn([]);
+
+        $this->clientMock->elastiCubes->getAll();
+    }
+
+    /**
+     * @covers \Sisense\Api\V09\ElastiCubes::getServers()
+     */
+    public function testGetServers()
+    {
+        $this->clientMock->expects($this->once())
+            ->method('runRequest')
+            ->with('elasticubes/servers', 'GET', ['query' => []])
+            ->willReturn([]);
+
+        $this->clientMock->elastiCubes->getServers();
+    }
+
+    /**
+     * @covers \Sisense\Api\V09\ElastiCubes::getServer()
+     */
+    public function testGetServer()
+    {
+        $this->clientMock->expects($this->once())
+            ->method('runRequest')
+            ->with('elasticubes/servers/serverName', 'GET', ['query' => []])
+            ->willReturn([]);
+
+        $this->clientMock->elastiCubes->getServer('serverName');
+    }
+
+    /**
+     * @covers \Sisense\Api\V09\ElastiCubes::getServerSimple()
+     */
+    public function testGetServerSimple()
+    {
+        $this->clientMock->expects($this->once())
+            ->method('runRequest')
+            ->with('elasticubes/servers/serverName/simple', 'GET', ['query' => []])
+            ->willReturn([]);
+
+        $this->clientMock->elastiCubes->getServerSimple('serverName');
+    }
+
+    /**
+     * @covers \Sisense\Api\V09\ElastiCubes::getServerStatus()
+     */
+    public function testGetServerStatus()
+    {
+        $this->clientMock->expects($this->once())
+            ->method('runRequest')
+            ->with('elasticubes/servers/serverName/status', 'GET', ['query' => []])
+            ->willReturn([]);
+
+        $this->clientMock->elastiCubes->getServerStatus('serverName');
+    }
+
+    /**
+     * @covers \Sisense\Api\V09\ElastiCubes::getDataSecurityRules()
+     */
+    public function testGetDataSecurityRules()
+    {
+        $this->clientMock->expects($this->once())
+            ->method('runRequest')
+            ->with('elasticubes/serverName/cubeName/datasecurity', 'GET', ['query' => []])
+            ->willReturn([]);
+
+        $this->clientMock->elastiCubes->getDataSecurityRules('serverName', 'cubeName');
+    }
+
+    /**
+     * @covers \Sisense\Api\V09\ElastiCubes::getUserDataSecurity()
+     */
+    public function testGetUserDataSecurity()
+    {
+        $this->clientMock->expects($this->once())
+            ->method('runRequest')
+            ->with('elasticubes/serverName/cubeName/userName/datasecurity', 'GET', ['query' => []])
+            ->willReturn([]);
+
+        $this->clientMock->elastiCubes->getUserDataSecurity('serverName', 'cubeName', 'userName');
+    }
+
+    /**
+     * @covers \Sisense\Api\V09\ElastiCubes::getColumnDataSecurity()
+     */
+    public function testGetColumnDataSecurity()
+    {
+        $this->clientMock->expects($this->once())
+            ->method('runRequest')
+            ->with('elasticubes/s/c/datasecurity/t/c', 'GET', ['query' => []])
+            ->willReturn([]);
+
+        $this->clientMock->elastiCubes->getColumnDataSecurity('s', 'c', 't', 'c');
+    }
+
+    /**
+     * @covers \Sisense\Api\V09\ElastiCubes::getAuthRecords()
+     */
+    public function testGetAuthRecords()
+    {
+        $this->clientMock->expects($this->once())
+            ->method('runRequest')
+            ->with('elasticubes/s/c/permissions', 'GET', ['query' => []])
+            ->willReturn([]);
+
+        $this->clientMock->elastiCubes->getAuthRecords('s', 'c');
+    }
+
+    /**
+     * @covers \Sisense\Api\V09\ElastiCubes::startServer()
+     */
+    public function testStartServer()
+    {
+        $this->clientMock->expects($this->once())
+            ->method('runRequest')
+            ->with('elasticubes/s/c/start', 'POST')
+            ->willReturn([]);
+
+        $this->clientMock->elastiCubes->startServer('s', 'c');
+    }
+
+    /**
+     * @covers \Sisense\Api\V09\ElastiCubes::stopServer()
+     */
+    public function testStopServer()
+    {
+        $this->clientMock->expects($this->once())
+            ->method('runRequest')
+            ->with('elasticubes/s/c/stop', 'POST')
+            ->willReturn([]);
+
+        $this->clientMock->elastiCubes->stopServer('s', 'c');
+    }
+
+    /**
+     * @covers \Sisense\Api\V09\ElastiCubes::restartServer()
+     */
+    public function testRestartServer()
+    {
+        $this->clientMock->expects($this->once())
+            ->method('runRequest')
+            ->with('elasticubes/s/c/restart', 'POST')
+            ->willReturn([]);
+
+        $this->clientMock->elastiCubes->restartServer('s', 'c');
+    }
+
+    /**
+     * @covers \Sisense\Api\V09\ElastiCubes::startBuild()
+     */
+    public function testStartBuild()
+    {
+        $this->clientMock->expects($this->once())
+            ->method('runRequest')
+            ->with('elasticubes/s/c/startBuild', 'POST', ['form_params' => ['type' => 't']])
+            ->willReturn([]);
+
+        $this->clientMock->elastiCubes->startBuild('s', 'c', 't');
+    }
+
+    /**
+     * @covers \Sisense\Api\V09\ElastiCubes::stopBuild()
+     */
+    public function testStopBuild()
+    {
+        $this->clientMock->expects($this->once())
+            ->method('runRequest')
+            ->with('elasticubes/s/c/stopBuild', 'POST', ['form_params' => []])
+            ->willReturn([]);
+
+        $this->clientMock->elastiCubes->stopBuild('s', 'c');
+    }
+
+    /**
+     * @covers \Sisense\Api\V09\ElastiCubes::executeJAQL()
+     */
+    public function testExecuteJAQL()
+    {
+        $this->clientMock->expects($this->once())
+            ->method('runRequest')
+            ->with('elasticubes/c/jaql', 'POST', ['form_params' => ['jaql' => 'jaql']])
+            ->willReturn([]);
+
+        $this->clientMock->elastiCubes->executeJAQL('jaql', 'c');
+    }
+
+    /**
+     * @covers \Sisense\Api\V09\ElastiCubes::defineDataSecurity()
+     */
+    public function testDefineDataSecurity()
+    {
+        $this->clientMock->expects($this->once())
+            ->method('runRequest')
+            ->with('elasticubes/s/c/datasecurity', 'POST', ['form_params' => ['body' => []]])
+            ->willReturn([]);
+
+        $this->clientMock->elastiCubes->defineDataSecurity('s', 'c', []);
+    }
+
+    /**
+     * @covers \Sisense\Api\V09\ElastiCubes::addDataSecurity()
+     */
+    public function testAddDataSecurity()
+    {
+        $this->clientMock->expects($this->once())
+            ->method('runRequest')
+            ->with('elasticubes/datasecurity', 'POST', ['form_params' => ['parameters']])
+            ->willReturn([]);
+
+        $this->clientMock->elastiCubes->addDataSecurity(['parameters']);
+    }
+
+    /**
+     * @covers \Sisense\Api\V09\ElastiCubes::attachDetachFolder()
+     */
+    public function testAttachDetachFolder()
+    {
+        $this->clientMock->expects($this->once())
+            ->method('runRequest')
+            ->with('elasticubes/s/c/attachDetach', 'POST', ['form_params' => ['body' => ['body']]])
+            ->willReturn([]);
+
+        $this->clientMock->elastiCubes->attachDetachFolder('s', 'c', ['body']);
+    }
+
+    /**
+     * @covers \Sisense\Api\V09\ElastiCubes::definePermission()
+     */
+    public function testDefinePermission()
+    {
+        $this->clientMock->expects($this->once())
+            ->method('runRequest')
+            ->with('elasticubes/s/c/permissions', 'POST', ['form_params' => ['shares' => ['shares']]])
+            ->willReturn([]);
+
+        $this->clientMock->elastiCubes->definePermission('s', 'c', ['shares']);
+    }
+
+    /**
+     * @covers \Sisense\Api\V09\ElastiCubes::updatePermissions()
+     */
+    public function testUpdatePermissions()
+    {
+        $this->clientMock->expects($this->once())
+            ->method('runRequest')
+            ->with('elasticubes/s/c/permissions', 'PUT', ['shares' => ['shares']])
+            ->willReturn([]);
+
+        $this->clientMock->elastiCubes->updatePermissions('s', 'c', ['shares']);
+    }
+
+    /**
+     * @covers \Sisense\Api\V09\ElastiCubes::deleteDataContext()
+     */
+    public function testDeleteDataContext()
+    {
+        $this->clientMock->expects($this->once())
+            ->method('runRequest')
+            ->with('elasticubes/s/c/datasecurity/t/c', 'DELETE', [])
+            ->willReturn([]);
+
+        $this->clientMock->elastiCubes->deleteDataContext('s', 'c', 't', 'c');
+    }
+
+    /**
+     * @covers \Sisense\Api\V09\ElastiCubes::deleteAllPermissions()
+     */
+    public function testDeleteAllPermissions()
+    {
+        $this->clientMock->expects($this->once())
+            ->method('runRequest')
+            ->with('elasticubes/s/c/datasecurity/t/c', 'DELETE', [])
+            ->willReturn([]);
+
+        $this->clientMock->elastiCubes->deleteAllPermissions('s', 'c', 't', 'c');
+    }
 }
